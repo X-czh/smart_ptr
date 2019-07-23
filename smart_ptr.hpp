f6a18eb873e47db74e7f1242ef68a6df699b4994
X-czh <m1a2@outlook.com>
2019-07-23 13:17:56 +0800

    update include smart_ptr

--- smart_ptr.hpp
+++ smart_ptr.hpp
@@ -1,8 +1,14 @@
+// include to use my own implementation of smart pointers
+
 #ifndef SMART_PTR_HPP
 #define SMART_PTR_HPP 1
 
 #include "src/unique_ptr.hpp"
 #include "src/shared_ptr.hpp"
 #include "src/weak_ptr.hpp"
+
+#include "default_delete.hpp"
+#include "bad_weak_ptr.hpp"
+#include "owner_less.hpp"
 
 #endif
